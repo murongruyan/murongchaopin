9a9c22c128a2d4f6dbf64b37381827923be59de5
murongruyan <murongruyan@gmail.com>
2025-12-10 02:00:13 +0800

    Fix action.sh to open module WebUI directly

--- action.sh
+++ action.sh
@@ -1,4 +1,4 @@
 #!/system/bin/sh
-# 启动 KSU WebUI
-am start -p io.github.a13e300.ksuwebui >/dev/null 2>&1
+# 启动 KSU WebUI 并跳转到本模块页面
+am start -n "io.github.a13e300.ksuwebui/.WebUIActivity" -e id "murongchaopin" >/dev/null 2>&1
 exit 0
